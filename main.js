e8b0752b475a2d60350920f3a4f7e95e46680d03
hasso13 <fuertesweb@gmail.com>
2025-01-14 02:32:39 +0100

    Create JS and finish app

--- main.js
+++ main.js
@@ -1,6 +1,31 @@
+const buttonsRanting = document.querySelectorAll(".buttons_wrapper button");
+const sendButton = document.querySelector(".send");
+const containerOne = document.querySelector(".section_wrapper");
+const containerTwo = document.querySelector(".section_two_wrapper");
+const changeRantingText = document.querySelector(".changeRanting");
+
+let changeRanting = null;
+
+buttonsRanting.forEach((button) => {
+    button.addEventListener("click", () => {
+        buttonsRanting.forEach((btn) => btn.classList.remove("active"));
+        button.classList.add("active");
+        changeRanting = button.textContent;
+    });
+});
+
+sendButton.addEventListener("click", () => {
+    if (changeRanting) {
+        containerOne.style.display = "none";
+
+        containerTwo.style.display='grid';
+        changeRantingText.textContent = `Seleccionaste ${changeRanting} de 5`;
+    }
+});
 
 
-const buttonsRanking = document.querySelectorAll('.buttons-wrapper button');
-const buttonSend = document.querySelector('.send')
+
+
+
 
 
